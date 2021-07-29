bf51b318d36c2703c6679c4c025eec370d3f5ffc
Brent Ahrens <cidida@gmail.com>
2021-07-28 21:28:48 -0700

    adds image tags to show pages

--- pages/number-station.tsx
+++ pages/number-station.tsx
@@ -1,32 +1,34 @@
+import Image from 'next/image'
+
 export default function numberStation() {
     return (
         <div className="page">
             <div className="page__banner">
-                <img src="/images/projects/number-station/number-station-main.png" />
+                <Image width={400} height={190} layout="responsive" src="/images/projects/number-station/number-station-main.png" />
             </div>
             <div className="page__content">
                 <h2>Context</h2>
                 <p>A new musical release by Brooklyn Rap Duo Young Planet, “Dog Days” is a 3 song EP that feels like a hot summer's day. Bouncy production and smooth lyrics paint pictures of trips to the beach, walking on a city street in 90 degree weather with a friend and falling in love.</p>
             </div>
             <div className="page__banner">
-                <img src="/images/projects/number-station/number-station-grid.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/number-station/number-station-grid.png" />
             </div>
             <div className="page__content">
                 <p>A new musical release by Brooklyn Rap Duo Young Planet, “Dog Days” is a 3 song EP that feels like a hot summer's day. Bouncy production and smooth lyrics paint pictures of trips to the beach, walking on a city street in 90 degree weather with a friend and falling in love.</p>
                 <h2>The Logo</h2>
             </div>
             <div className="page__image">
-                <img src="/images/projects/number-station/number-station-2.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/number-station/number-station-2.png" />
             </div>
             <div className="page__image last">
-                <img src="/images/projects/number-station/number-station-3.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/number-station/number-station-3.png" />
             </div>
             <div className="page__content">
                 <h2>Context</h2>
                 <p>A new musical release by Brooklyn Rap Duo Young Planet, “Dog Days” is a 3 song EP that feels like a hot summer's day. Bouncy production and smooth lyrics paint pictures of trips to the beach, walking on a city street in 90 degree weather with a friend and falling in love.</p>
             </div>
             <div className="page__banner">
-                <img src="/images/projects/number-station/number-station-4.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/number-station/number-station-4.png" />
             </div>
         </div>
     )
--- pages/prickly-pear.tsx
+++ pages/prickly-pear.tsx
@@ -1,37 +1,39 @@
+import Image from 'next/image'
+
 export default function numberStation() {
     return (
         <div className="page">
             <div className="page__banner">
-                <img src="/images/projects/prickly-pear/pp-banner.png" />
+                <Image width={400} height={190} layout="responsive" src="/images/projects/prickly-pear/pp-banner.png" />
             </div>
             <div className="page__content">
                 <h2>Context</h2>
                 <p>Prickly Pear is a plant nursery in Montreal, Quebec. They were founded in 2014 by two sisters who studied Agriculture and Biology in university. They named the nursery after their favourite cactus, the prickly pear.  Their target audience is young to middle-aged city dwellers that dream of turning their apartments into lush urban jungles. After 5 years in business, the owners had brand elements they’d like to retain and ones they’re ready to let go of, and wanted to rebrand their company to better represent their product offering.</p>
             </div>
             <div className="page__banner">
-                <img src="/images/projects/prickly-pear/pp-2.png" />
+                <Image width={400} height={260} layout="responsive" src="/images/projects/prickly-pear/pp-2.png" />
             </div>
             <div className="page__content">
                 <h2>Concept</h2>
                 <p>The re-branded visual identity is centered around the flower that the Prickly Pear Cactus bears, and the logo is paired together with a wordmark that has a sharp / prickly quality to itself. Using a more organic shape to construct the logo we’re able to size up or down way more confidently compared to the original detailed illustration. The custom graphic elements were also re-designed to have the same filled feel in comparison to the original half outlined half filled assets.</p>
             </div>
             <div className="page__image">
-                <img src="/images/projects/prickly-pear/pp-3.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/prickly-pear/pp-3.png" />
             </div>
             <div className="page__image">
-                <img src="/images/projects/prickly-pear/pp-4.png" />
+                <Image width={450} height={160} layout="responsive" src="/images/projects/prickly-pear/pp-4.png" />
             </div>
             <div className="page__image">
-                <img src="/images/projects/prickly-pear/pp-1.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/prickly-pear/pp-1.png" />
             </div>
             <div className="page__image">
-                <img src="/images/projects/prickly-pear/pp-7.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/prickly-pear/pp-7.png" />
             </div>
             <div className="page__image">
-                <img src="/images/projects/prickly-pear/pp-5.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/prickly-pear/pp-5.png" />
             </div>
             <div className="page__banner">
-                <img src="/images/projects/prickly-pear/pp-1.png" />
+                <Image width={400} height={280} layout="responsive" src="/images/projects/prickly-pear/pp-1.png" />
             </div>
         </div>
     )
