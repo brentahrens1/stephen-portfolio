e60be303d97ac1969c6c1741795b6f3939c109f1
Brent Ahrens <cidida@gmail.com>
2021-08-17 09:45:38 -0700

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Layout>
         <Component {...pageProps} />
